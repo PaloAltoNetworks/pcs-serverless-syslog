7d2c8b6e170bc45a67433599f06cd8163cf621c3
MPH13 <marcphobson@gmail.com>
2019-10-30 09:43:50 -0700

    webhook syslog if then else

--- cloudfunction.py
+++ cloudfunction.py
@@ -1,15 +1,39 @@
+"""
+A cloud function to forward Prisma Cloud webhook messages to syslog
+"""
+#
+# main() will be run when you invoke this action
+#
+
 import sys
 import logging
 import logging.handlers
 import socket
- 
- SYSLOG_HOST = "123.456.789.101"
- SYSLOG_PORT = 514
-  
-  def main(request):
-          request_json = request.get_json(silent=True)
-              syslogger = logging.getLogger('MySysLogger')
-                  syslogger.setLevel(logging.INFO)
-                      handler = logging.handlers.SysLogHandler(address = (SYSLOG_HOST, SYSLOG_PORT),facility =logging.handlers.SysLogHandler.LOG_LOCAL3,socktype=socket.SOCK_STREAM)
-                          syslogger.addHandler(handler)
-                              syslogger.info(request_json)
+import json
+import requests
+
+#Select either Syslog or Webhook destination
+#OUTPUT = <webhook or syslog>
+OUTPUT = "syslog"
+
+#Insert Syslog server info 
+SYSLOG_HOST = "34.69.181.79"
+SYSLOG_PORT = 514
+
+#Insert Webhook URL information 
+#webhook_url = 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX'
+
+
+
+def main(request):
+	request_json = request.get_json(silent=True)
+    if(OUTPUT == "syslog"): 
+	    syslogger = logging.getLogger('MySysLogger')
+	    syslogger.setLevel(logging.INFO)
+	    handler = logging.handlers.SysLogHandler(address = (SYSLOG_HOST, SYSLOG_PORT),facility = logging.handlers.SysLogHandler.LOG_LOCAL3,socktype=socket.SOCK_STREAM)
+	    syslogger.addHandler(handler)
+	    for alert in range(len(request_json)):
+       		syslogger.info(json.dumps(request_json[alert]))
+	elif(OUTPUT == webhook):
+		for alert in range(len(request_json)):
+       		response = requests.post(webhook_url, data=json.dumps(request_json[alert]),headers={'Content-Type': 'application/json'})
