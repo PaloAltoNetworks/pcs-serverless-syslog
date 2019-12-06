9691245fad2c580b09a831ae6f8e3e64fb89b10e
Marc Hobson <45016646+MPH13@users.noreply.github.com>
2019-12-06 15:43:32 -0800

    Update cloudfunction.py

--- cloudfunction.py
+++ cloudfunction.py
@@ -1,42 +1,39 @@
 """
 A cloud function to forward Prisma Cloud webhook messages to syslog
-Written by
-Eddie Beuerlein
-Marc Hobson
 """
 #
 # main() will be run when you invoke this action
 #
 
-import sys
 import logging
+import sys
 import logging.handlers
 import socket
 import json
 import requests
 
-#Select either Syslog or Webhook destination
-#OUTPUT = <webhook or syslog>
-OUTPUT = "syslog"
-
-#Insert Syslog server info 
-SYSLOG_HOST = "XXX.XXX.XXX.XXX"
+SYSLOG_HOST = "34.69.181.79"
 SYSLOG_PORT = 514
-
-#Insert Webhook URL information 
-#webhook_url = 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX'
-
-
+DEST_WEBHOOK = "https://webhook.site/79a10feb-a432-475b-87c2-d85ce129a1d9"
+#Set to SYSLOG or WEBHOOK
+DEST_TYPE = "SYSLOG"
 
 def main(request):
-	request_json = request.get_json(silent=True)
-    if(OUTPUT == "syslog"): 
-	    syslogger = logging.getLogger('MySysLogger')
-	    syslogger.setLevel(logging.INFO)
-	    handler = logging.handlers.SysLogHandler(address = (SYSLOG_HOST, SYSLOG_PORT),facility = logging.handlers.SysLogHandler.LOG_LOCAL3,socktype=socket.SOCK_STREAM)
-	    syslogger.addHandler(handler)
-	    for alert in range(len(request_json)):
-       		syslogger.info(json.dumps(request_json[alert]))
-	elif(OUTPUT == webhook):
-		for alert in range(len(request_json)):
-       		response = requests.post(webhook_url, data=json.dumps(request_json[alert]),headers={'Content-Type': 'application/json'})
+    request_json = request.get_json(silent=True)
+    #Use below for SYSLOG
+    if (DEST_TYPE=="SYSLOG"):
+        syslogger = logging.getLogger('MySysLogger')
+        syslogger.setLevel(logging.INFO)
+        handler = logging.handlers.SysLogHandler(address = (SYSLOG_HOST, SYSLOG_PORT),facility = logging.handlers.SysLogHandler.LOG_LOCAL3,socktype=socket.SOCK_STREAM)
+        handler.setFormatter(logging.Formatter('%(message)s\n'))
+        handler.append_nul = False
+        syslogger.addHandler(handler)    
+        for alert in range(len(request_json)):
+            syslogger.info(json.dumps(request_json[alert]))
+    #Else use below for WEBHOOK
+    elif (DEST_TYPE=="WEBHOOK"):
+        for alert in range(len(request_json)):
+            s = requests.Session()
+            response = s.post(DEST_WEBHOOK, data=json.dumps(request_json[alert]), headers={'Content-Type': 'application/json'})   
+            if response.status_code != 200:
+                raise ValueError('Request to Webhook returned an error %s, the response is:%s' % (response.status_code, response.text))
